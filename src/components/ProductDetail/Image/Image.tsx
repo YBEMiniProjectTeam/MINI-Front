fbae082d4a43d8c87cef8b6dcdd7474e4d5bb125
lilviolie <lilviolie@gmail.com>
2023-11-23 04:13:58 +0900

    Feat: Image 컴포넌트 추가

---
+++ src/components/ProductDetail/Image/Image.tsx
@@ -0,0 +1,98 @@
+import { useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton
+} from "@chakra-ui/react";
+
+export const Image = ({
+  images
+}: {
+  images: { url: string }[];
+}): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const imageUrl1 = images[0]?.url;
+  const imageUrl2 = images[1]?.url;
+  const imageUrl3 = images[2]?.url;
+
+  return (
+    <Box w="720px" p={0} margin="0 auto">
+      <Grid
+        templateColumns="1.7fr 1fr"
+        autoRows="190px"
+        gap={1}
+        position="relative"
+      >
+        <GridItem
+          bgImage={imageUrl1 || undefined}
+          bgSize="cover"
+          bgPosition="center"
+          gridColumn="1 / 2"
+          gridRow="1 / 3"
+          _hover={{
+            filter: "brightness(90%)"
+          }}
+        />
+        <GridItem
+          bgImage={imageUrl2 || undefined}
+          bgSize="cover"
+          bgPosition="center"
+          gridColumn={2}
+          gridRow={1}
+          _hover={{
+            filter: "brightness(90%)"
+          }}
+        />
+        <GridItem
+          bgImage={imageUrl3 || undefined}
+          bgSize="cover"
+          bgPosition="center"
+          gridColumn={2}
+          gridRow="2 / 3"
+          _hover={{
+            filter: "brightness(90%)"
+          }}
+        />
+        <Button
+          bg="white"
+          variant="outline"
+          size="sm"
+          position="absolute"
+          right="20px"
+          bottom="20px"
+          onClick={onOpen}
+        >
+          사진 모두 보기
+        </Button>
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent maxW="720px" maxH="600px" overflow="auto">
+            <ModalHeader>이미지 전체보기</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              {images?.map((image, index) => (
+                <Box
+                  key={index}
+                  bgImage={image.url}
+                  bgSize="cover"
+                  bgPosition="center"
+                  w="100%"
+                  h="300px"
+                  marginBottom="20px"
+                />
+              ))}
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Grid>
+    </Box>
+  );
+};
--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAccomodationInfo } from "../../api/accomodation/getAccomodationInfo";
+import { Image } from "../../components/ProductDetail/Image/Image";
+import { Accomodation } from "../../components/ProductDetail/ProductDetail.types";
 
-export const ProductDetail = () => {
-  return <div>productDetail</div>;
+export const ProductDetail: React.FC = () => {
+  const [accomodationInfo, setAccomodationInfo] = useState<Accomodation>();
+  const images = accomodationInfo?.accomodation_image || [];
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const data = await getAccomodationInfo();
+      setAccomodationInfo(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return <Image images={images} />;
 };
