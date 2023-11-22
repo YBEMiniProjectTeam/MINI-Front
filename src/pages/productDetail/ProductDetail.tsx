a07ce147ecfe833ee06dce78f67fd57276337c4a
lilviolie <lilviolie@gmail.com>
2023-11-23 04:50:59 +0900

    Feat: 숙소 타입 및 이름 추가

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { getAccomodationInfo } from "../../api/accomodation/getAccomodationInfo";
 import { Image } from "../../components/ProductDetail/Image/Image";
 import { Accomodation } from "../../components/ProductDetail/ProductDetail.types";
+import { Box } from "@chakra-ui/react";
 
 export const ProductDetail: React.FC = () => {
   const [accomodationInfo, setAccomodationInfo] = useState<Accomodation>();
   const images = accomodationInfo?.accomodation_image || [];
+  const { type, name } = accomodationInfo || {};
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -16,5 +18,15 @@ export const ProductDetail: React.FC = () => {
     fetchData();
   }, []);
 
-  return <Image images={images} />;
+  return (
+    <>
+      <Image images={images} />
+      <Box marginTop="30px">
+        <Box fontSize="14px">{type}</Box>
+        <Box fontSize="30px" fontWeight={700}>
+          {name}
+        </Box>
+      </Box>
+    </>
+  );
 };
