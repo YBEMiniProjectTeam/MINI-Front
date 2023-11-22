ab7036c6fc0ab772af1532da5fa9b52b4dc420e3
YongYong21 <imkinggosu@gmail.com>
2023-11-22 14:55:58 +0900

    Change: Name apiUrl to API_URL

--- src/api/login/LoginApi.ts
+++ src/api/login/LoginApi.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 import { Login } from "../../components/Login/Login.types";
 
 export const LoginApi = (login: Login): void => {
-  const apiUrl =
+  const API_URL =
     "https://cca6d5da-811b-445f-8811-62cab12d0157.mock.pstmn.io/login";
   const headers = {
     "Content-Type": "application/json"
   };
 
   axios
-    .post(apiUrl, login, { headers })
+    .post(API_URL, login, { headers })
     .then((res) => {
       console.log(res);
     })
--- src/api/register/RegisterApi.ts
+++ src/api/register/RegisterApi.ts
@@ -3,14 +3,14 @@ import Swal from "sweetalert2";
 import { User } from "../../components/Register/Register.types";
 
 export const RegisterApi = (user: User): void => {
-  const apiUrl =
+  const API_URL =
     "https://cca6d5da-811b-445f-8811-62cab12d0157.mock.pstmn.io/sign-up";
   const headers = {
     "Content-Type": "application/json"
   };
 
   axios
-    .post(apiUrl, user, { headers })
+    .post(API_URL, user, { headers })
     .then((res) => {
       console.log(res);
       Swal.fire("회원가입이 정상처리되었습니다.");
