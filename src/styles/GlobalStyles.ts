ff7fc1c30f07ca36ffd37a3f75e3decf18fbb3d9
lilviolie <lilviolie@gmail.com>
2023-11-23 04:43:12 +0900

    Design: 폰트 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+    }
+
     #root {
     width: 768px;
     height: 100vh;
@@ -18,6 +25,7 @@ const GlobalStyles = createGlobalStyle`
     }
     *{
         box-sizing: border-box;
+        font-family: 'Pretendard';
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
     a, dl, dt, dd, ol, ul, li, form, label, table{
