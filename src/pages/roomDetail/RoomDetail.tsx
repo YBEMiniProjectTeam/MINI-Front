504e1a637cf7cf134eb4d8988ebbc4dd9970a6fb
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-22 19:11:12 +0900

    Feat: 객실 상세정보 페이지 마크업

---
+++ src/pages/roomDetail/RoomDetail.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import * as styles from "./RoomDetail.styles";
+import RoomCarousel from "../../components/RoomCarousel/RoomCarousel.tsx";
+import { useRoomDetail } from "../../hooks/useRoomDetailQuery";
+import RoomHeader from "../../components/RoomHeader/RoomHeader.tsx";
+import { Button } from "@chakra-ui/react";
+import RoomCard from "../../components/RoomCard/RoomCard";
+import RoomInfoSection from "../../components/RoomInfoSection/RoomInfoSection.tsx";
+import { dummyData, dummyData1, dummyData2 } from "./RoomDetail.data.ts";
+
+export const RoomDetail: React.FC = () => {
+  const { roomId } = useParams();
+  const navigate = useNavigate();
+
+  const { data, error, isLoading } = useRoomDetail(roomId!);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleReservationClick = () => {
+    navigate(`/order/${roomId}`, {
+      state: {
+        roomId,
+        name: data.name,
+        price: data.price,
+        capacity: data.capacity,
+        capacityMax: data.capacity_max
+      }
+    });
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error has occurred: {error.message}</div>;
+
+  return (
+    <styles.Container>
+      <styles.Inner>
+        <styles.Wrapper>
+          <RoomCarousel images={data.room_image} onClick={openModal} />
+          <RoomHeader
+            name={data.name}
+            capacity={data.capacity}
+            capacityMax={data.capacity_max}
+            introMessage={data.intro_message}
+          />
+        </styles.Wrapper>
+        <styles.Divider />
+        <styles.Label>
+          <h2>상품 선택</h2>
+        </styles.Label>
+        <RoomCard
+          price={data.price}
+          capacity={data.capacity}
+          capacityMax={data.capacity_max}
+          stockQuantity={data.stock_quantity}
+        >
+          <Button
+            colorScheme="pink"
+            disabled={data.stock_quantity <= 0}
+            onClick={handleReservationClick}
+          >
+            {data.stock_quantity > 0 ? "예약하기" : "예약 마감"}
+          </Button>
+        </RoomCard>
+        <styles.Divider />
+        <styles.Label>
+          <h2>편의시설 및 서비스</h2>
+        </styles.Label>
+        <RoomInfoSection title="서비스" content={dummyData} />
+        <styles.Divider />
+        <styles.Label>
+          <h2>객실 정보</h2>
+        </styles.Label>
+        <RoomInfoSection title="객실 정보" content={dummyData1} />
+        <styles.Divider />
+        <styles.Label>
+          <h2>예약공지사항</h2>
+        </styles.Label>
+        <RoomInfoSection title="예약공지사항" content={dummyData2} />
+      </styles.Inner>
+    </styles.Container>
+  );
+};
