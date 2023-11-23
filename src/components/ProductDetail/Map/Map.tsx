ee1e84f5b13edba8ccff9c02ffa36e720ec93390
lilviolie <lilviolie@gmail.com>
2023-11-23 21:44:18 +0900

    Feat: Map 컴포넌트 추가

---
+++ src/components/ProductDetail/Map/Map.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { Box } from "@chakra-ui/react";
+
+export const Map = ({ lat, lng }: { lat: number; lng: number }) => {
+  const position = { lat, lng };
+
+  useEffect(() => {
+    let map: google.maps.Map;
+    async function initMap(): Promise<void> {
+      //@ts-ignore
+      const { Map } = (await google.maps.importLibrary(
+        "maps"
+      )) as google.maps.MapsLibrary;
+      const { Marker } = (await google.maps.importLibrary(
+        "marker"
+      )) as google.maps.MarkerLibrary;
+
+      map = new Map(document.getElementById("map") as HTMLElement, {
+        center: position,
+        zoom: 15
+      });
+
+      const marker = new Marker({
+        map: map,
+        position: position
+      });
+    }
+
+    initMap();
+  }, []);
+
+  return <Box id="map" w="100%" minH="300px" margin="10px 0 40px" />;
+};
--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -4,8 +4,10 @@ import { Image } from "../../components/ProductDetail/Image/Image";
 import { WishListButton } from "../../components/ProductDetail/WishListButton/WishListButton";
 import { ChooseDetail } from "../../components/ProductDetail/ChooseDetail/ChooseDetail";
 import { ChooseRoom } from "../../components/ProductDetail/ChooseRoom/ChooseRoom";
+import { Map } from "../../components/ProductDetail/Map/Map";
 import { Accomodation } from "../../components/ProductDetail/ProductDetail.types";
 import { Flex, Box, Divider } from "@chakra-ui/react";
+import { IoLocationOutline } from "react-icons/io5";
 
 export const ProductDetail: React.FC = () => {
   const [accomodationInfo, setAccomodationInfo] = useState<Accomodation>();
@@ -49,6 +51,24 @@ export const ProductDetail: React.FC = () => {
       </Flex>
       <ChooseDetail />
       <ChooseRoom />
+      <Divider margin="40px 0" borderColor="#D9D9D9" />
+      <Flex flexDir="column" alignItems="flex-start" gap="5px">
+        <Box fontSize="26px" fontWeight={700}>
+          숙소 위치
+        </Box>
+        <Flex
+          alignItems="center"
+          gap="5px"
+          fontSize="16px"
+          fontWeight={500}
+          color="#7F7F7F"
+          marginBottom="5px"
+        >
+          <IoLocationOutline />
+          경상남도 남해군 설천면 노량로 194
+        </Flex>
+      </Flex>
+      <Map lat={34.93929248} lng={127.8740927} />
     </>
   );
 };
