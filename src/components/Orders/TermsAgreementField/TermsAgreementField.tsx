b5c1a49969950aaf0da94bb9438423de6e668519
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-25 01:25:50 +0900

    fix: 체크박스 유효성 검사 로직 수정

--- src/components/Orders/TermsAgreementField/TermsAgreementField.tsx
+++ src/components/Orders/TermsAgreementField/TermsAgreementField.tsx
@@ -1,34 +1,51 @@
 import React from "react";
+import { useFormContext, Controller } from "react-hook-form";
 import * as styles from "./TermsAgreementField.styles";
 
 const TermsAgreementField: React.FC = () => {
+  const { control } = useFormContext();
   const [checkedItems, setCheckedItems] = React.useState([false, false]);
 
   const allChecked = checkedItems.every(Boolean);
-  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
 
   return (
     <styles.Form>
-      <styles.StyledCheckbox
-        isChecked={allChecked}
-        isIndeterminate={isIndeterminate}
-        onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
-      >
-        <styles.Title>필수 약관 전체 동의</styles.Title>
-      </styles.StyledCheckbox>
+      <Controller
+        name="termsAgreement"
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange, ref } }) => (
+          <styles.StyledCheckbox
+            ref={ref}
+            isChecked={allChecked}
+            onChange={(e) => {
+              const newChecked = e.target.checked;
+              onChange(newChecked);
+              setCheckedItems([newChecked, newChecked]);
+            }}
+          >
+            <styles.Title>필수 약관 전체 동의</styles.Title>
+          </styles.StyledCheckbox>
+        )}
+      />
       <styles.Stack>
-        <styles.StyledCheckbox
-          isChecked={checkedItems[0]}
-          onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
-        >
-          <styles.Item>[필수] 개인정보 수집 및 이용</styles.Item>
-        </styles.StyledCheckbox>
-        <styles.StyledCheckbox
-          isChecked={checkedItems[1]}
-          onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
-        >
-          <styles.Item>[필수] 개인정보 제 3자 제공</styles.Item>
-        </styles.StyledCheckbox>
+        {checkedItems.map((checked, index) => (
+          <styles.StyledCheckbox
+            key={index}
+            isChecked={checked}
+            onChange={(e) => {
+              const newCheckedItems = [...checkedItems];
+              newCheckedItems[index] = e.target.checked;
+              setCheckedItems(newCheckedItems);
+            }}
+          >
+            <styles.Item>
+              {index === 0
+                ? "[필수] 개인정보 수집 및 이용"
+                : "[필수] 개인정보 제 3자 제공"}
+            </styles.Item>
+          </styles.StyledCheckbox>
+        ))}
       </styles.Stack>
     </styles.Form>
   );
