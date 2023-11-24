6c56113ddbebeb058cd9214fa4e6bd05027b23fa
lilviolie <lilviolie@gmail.com>
2023-11-25 06:33:09 +0900

    Design: 폰트 삭제

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,13 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard.woff') format('woff');
-        font-weight: 400;
-        font-style: normal;
-    }
-
     #root {
     width: 768px;
     margin: 0 auto;
@@ -24,7 +17,6 @@ const GlobalStyles = createGlobalStyle`
     }
     *{
         box-sizing: border-box;
-        font-family: 'Pretendard';
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
     a, dl, dt, dd, ol, ul, li, form, label, table{
