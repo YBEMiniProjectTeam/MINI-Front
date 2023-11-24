f78151554c4f9541c0e660816e74faa4edf17b85
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-24 10:59:49 +0900

    Feat: 유저 정보 카드 마크업

---
+++ src/components/Orders/UserInfoField/UserInfoField.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const UserInfoContainer = styled.div`
+  padding: 1rem 1rem 1.6rem;
+`;
+
+export const UserInfoWrapperRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.8rem;
+  padding: 0.3rem 0;
+
+  &:last-child {
+    margin: 0;
+  }
+`;
+export const UserInfoLabel = styled.div`
+  flex: auto;
+  font-size: 0.9rem;
+`;
+export const UserInfoItem = styled.div`
+  flex: 5;
+  justify-content: flex-end;
+  display: flex;
+  font-size: 0.9rem;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
