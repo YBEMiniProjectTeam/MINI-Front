021025bca11f83e1f05d34c339906fcd9c841dcd
lilviolie <lilviolie@gmail.com>
2023-11-24 18:07:44 +0900

    Feat: 날짜 선택 버튼 모달 연동

--- src/components/ProductDetail/ChooseDetail/ChooseDetail.tsx
+++ src/components/ProductDetail/ChooseDetail/ChooseDetail.tsx
@@ -1,38 +1,65 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import ChooseDateModal from "@/components/ChooseDateModal/ChooseDateModal";
+import { Box, Flex, useDisclosure } from "@chakra-ui/react";
 import { CiCalendar } from "react-icons/ci";
 import { BsPeople } from "react-icons/bs";
+import { convertDateFormat } from "@/utils/convertDateFormat";
 
 export const ChooseDetail = (): JSX.Element => {
+  const [selectedDate, setSelectedDate] = useState<string[] | undefined>([]);
+  const {
+    isOpen: isOpenChooseDateModal,
+    onOpen: onOpenChooseDateModal,
+    onClose: onCloseChooseDateModal
+  } = useDisclosure();
+
   return (
-    <Box marginTop="10px">
-      <Flex gap="10px">
-        <Box
-          as="button"
-          display="flex"
-          alignItems="center"
-          gap="5px"
-          bg="white"
-          borderRadius="25px"
-          border="1px solid #D9D9D9"
-          padding="10px 17px"
-        >
-          <CiCalendar />
-          11.22-11.23, 1박
-        </Box>
-        <Box
-          as="button"
-          display="flex"
-          alignItems="center"
-          gap="5px"
-          bg="white"
-          borderRadius="25px"
-          border="1px solid #D9D9D9"
-          padding="10px 17px"
-        >
-          <BsPeople />
-          2명
-        </Box>
-      </Flex>
-    </Box>
+    <>
+      <ChooseDateModal
+        isOpen={isOpenChooseDateModal}
+        onClose={onCloseChooseDateModal}
+        setSelectedDate={setSelectedDate}
+        isFromSearchResult={false}
+      />
+      <Box marginTop="10px">
+        <Flex gap="10px">
+          <Box
+            as="button"
+            display="flex"
+            alignItems="center"
+            gap="5px"
+            bg="white"
+            borderRadius="25px"
+            border="1px solid #D9D9D9"
+            padding="10px 17px"
+            color="#4B4B4B"
+            fontWeight={600}
+            onClick={onOpenChooseDateModal}
+          >
+            <CiCalendar />
+            {selectedDate &&
+              `${convertDateFormat(selectedDate[0])} - ${convertDateFormat(
+                selectedDate[1]
+              )}`}
+          </Box>
+          <Box
+            as="button"
+            display="flex"
+            alignItems="center"
+            gap="5px"
+            bg="white"
+            borderRadius="25px"
+            border="1px solid #D9D9D9"
+            padding="10px 17px"
+            color="#4B4B4B"
+            fontWeight={600}
+            onClick={onOpenChooseDateModal}
+          >
+            <BsPeople />
+            2명
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 };
