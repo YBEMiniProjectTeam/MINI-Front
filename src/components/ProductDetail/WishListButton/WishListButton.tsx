901cac21d423357d9cd3d1a8cb37345f986b7302
lilviolie <lilviolie@gmail.com>
2023-11-27 06:56:24 +0900

    Design: 위시리스트 버튼 마크업 수정

--- src/components/ProductDetail/WishListButton/WishListButton.tsx
+++ src/components/ProductDetail/WishListButton/WishListButton.tsx
@@ -1,17 +1,44 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { GoHeart } from "react-icons/go";
+import { GoHeartFill } from "react-icons/go";
 
 const WishListButton = (): JSX.Element => {
   return (
-    <Button
-      leftIcon={<GoHeart />}
-      bg="white"
-      variant="outline"
-      outline="none"
-      size="lg"
-    >
-      5,294
-    </Button>
+    <>
+      <Box
+        as="button"
+        display="flex"
+        alignItems="center"
+        gap="5px"
+        marginTop="14px"
+        padding="5px 10px"
+        borderRadius="10px"
+        transition="0.2s"
+        _hover={{
+          bg: "#F7F7F7"
+        }}
+      >
+        <GoHeart />
+        저장
+      </Box>
+      {/* 위시리스트에 있는 경우 */}
+      {/* <Box
+        as="button"
+        display="flex"
+        alignItems="center"
+        gap="5px"
+        marginTop="14px"
+        padding="5px 10px"
+        borderRadius="10px"
+        transition="0.2s"
+        _hover={{
+          bg: "#F7F7F7"
+        }}
+      >
+        <GoHeartFill color="#D53F8C" />
+        취소
+      </Box> */}
+    </>
   );
 };
 
