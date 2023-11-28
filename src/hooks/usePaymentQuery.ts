418cb08f27120c79b58c78f6eaffbad84c157440
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-29 02:00:09 +0900

    Feat: 예약하기 리액트쿼리 로직 변경

--- src/hooks/usePaymentQuery.ts
+++ src/hooks/usePaymentQuery.ts
@@ -1,33 +1,68 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { getPaymentInfo } from "@api/getPaymentInfo";
 
-type PaymentData = {
-  reservationData: { label: string; value: string }[];
-  rawData: any;
+interface RoomInfo {
+  cartId: number;
+  quantity: number;
+  roomName: string;
+  accommodationThumbnailUrl: string;
+  price: number;
+  checkInDate: string;
+  checkOutDate: string;
+  checkInTime: string;
+  checkOutTime: string;
+  capacity: number;
+  capacityMax: number;
+  discount?: number;
+}
+
+interface Accommodation {
+  accommodationName: string;
+  address: string;
+  roomInfos: RoomInfo[];
+}
+
+interface PaymentResponse {
+  reservationData: { label: string; value: number }[];
+  rawData: Accommodation[];
+}
+
+const encodeReservationData = (
+  data: Accommodation[]
+): { label: string; value: number }[] => {
+  let totalPrice = 0;
+  let totalDiscount = 0;
+
+  data.forEach((accommodation) => {
+    accommodation.roomInfos.forEach((roomInfo) => {
+      totalPrice += roomInfo.price;
+      totalDiscount += roomInfo.discount || 0;
+    });
+  });
+
+  return [
+    { label: "총 상품 금액", value: totalPrice },
+    { label: "총 할인 금액", value: totalDiscount }
+  ];
 };
 
-export const usePayment = (orderId: string) => {
-  return useSuspenseQuery<PaymentData>({
-    queryKey: ["payment", orderId],
-    queryFn: async () => {
-      const response = await axios.get(`/api/orders/${orderId}`);
-      return response.data;
-    },
-    select: (rawData) => {
+export const usePayment = (
+  cartIds: number[],
+  headers: { [key: string]: string }
+) => {
+  return useSuspenseQuery<
+    Accommodation[],
+    Error,
+    PaymentResponse,
+    [string, number[]]
+  >({
+    queryKey: ["payment", cartIds],
+    queryFn: async () => await getPaymentInfo(cartIds, headers),
+    select: (accommodations: Accommodation[]): PaymentResponse => {
       return {
-        reservationData: encodeReservationData(rawData),
-        rawData
+        reservationData: encodeReservationData(accommodations),
+        rawData: accommodations
       };
     }
   });
-};
-
-const encodeReservationData = (data: any) => {
-  return [
-    { label: "상품 금액", value: data.data.price },
-    {
-      label: "할인 금액",
-      value: data.data.discount || 0
-    }
-  ];
 };
