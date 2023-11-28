3d97f6aa0049598136d0d6b8947044700f2e35f6
lilviolie <lilviolie@gmail.com>
2023-11-28 17:01:43 +0900

    Fix: API 요청 주소 수정

--- src/api/accomodation/getRoomList.ts
+++ src/api/accomodation/getRoomList.ts
@@ -5,7 +5,7 @@ const getRoomList = async (
   endDate: string,
   guestNum: number
 ) => {
-  const GET_ROOM_LIST_URL = `https://anti-bias.kr/api/accommodations/1/rooms?start_date=${startDate}&end_date=${endDate}&guest_num=${guestNum}`;
+  const GET_ROOM_LIST_URL = `https://api.anti-bias.kr/api/accommodations/1/rooms?start_date=${startDate}&end_date=${endDate}&guest_num=${guestNum}`;
   const response = await axios.get(GET_ROOM_LIST_URL);
 
   return response.data.data.rooms;
