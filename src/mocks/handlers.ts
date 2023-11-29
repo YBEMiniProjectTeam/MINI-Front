9cdd656bc523bb8e5b240c08eac260c2838488fd
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-29 23:11:02 +0900

    Feat: 결제 정보 더미데이터 추가 및 핸들러 설정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { http, HttpResponse } from "msw";
 import dummyPaymentData from "./data/dummyPaymentData.json";
 import dummyMemberinfoData from "./data/dummyMemberInfoData.json";
+import dummyCompletedPaymentData from "./data/dummyCompletedPaymentData.json";
 
 export const handlers = [
   http.post(`/api/carts/orders/reservations`, () => {
@@ -8,5 +9,8 @@ export const handlers = [
   }),
   http.get(`/api/member-info`, () => {
     return HttpResponse.json(dummyMemberinfoData);
+  }),
+  http.get(`/api/carts/orders/complete`, () => {
+    return HttpResponse.json(dummyCompletedPaymentData);
   })
 ];
