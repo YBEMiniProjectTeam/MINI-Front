36b85c6cd3f02715fad21e002e92545a40d77714
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-29 23:13:31 +0900

    Fix: 결제완료 페이지 리액트쿼리 로직 수정

--- src/hooks/useCompletedPaymentQuery.ts
+++ src/hooks/useCompletedPaymentQuery.ts
@@ -1,35 +1,83 @@
 import { getCompletedPayment } from "@api/getCompletedPayment.ts";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
-type CompletePaymentData = {
-  reservationData: { label: string; value: string }[];
-  rawData: any;
+export interface Reservation {
+  reservation_name: string;
+  accommodation_name: string;
+  room_info: RoomInfo;
+}
+
+export interface RoomInfo {
+  room_name: string;
+  price: number;
+  check_in: string;
+  check_out: string;
+  capacity: number;
+  capacity_max: number;
+}
+
+export interface ReservationData {
+  key: "label" | "price";
+  label: string;
+  value: string | number;
+}
+
+interface CompletePaymentResponse {
+  reservationData: ReservationData[][];
+  reservationName: string;
+  totalPrice: number;
+  rawData: Reservation[];
+}
+
+const encodeReservationData = (
+  reservations: Reservation[]
+): ReservationData[][] => {
+  return reservations.map((reservation) => [
+    {
+      key: "label",
+      label: "숙소",
+      value: reservation.accommodation_name
+    },
+    {
+      key: "label",
+      label: "객실",
+      value: reservation.room_info.room_name
+    },
+    {
+      key: "price",
+      label: "결제 금액",
+      value: reservation.room_info.price
+    }
+  ]);
+};
+
+const encodeTotalPrice = (reservations: Reservation[]) => {
+  const totalPrice = reservations.reduce((total, reservation) => {
+    return total + reservation.room_info.price;
+  }, 0);
+
+  return totalPrice;
+};
+
+const encodeReservationName = (reservations: Reservation[]): string => {
+  const reservationName = reservations[0]?.reservation_name || "N/A";
+  return reservationName;
 };
 
 export const useCompletedPayment = (
-  cartIds: number[],
-  reservationName: string,
+  count: number,
   headers: { [key: string]: string }
 ) => {
-  return useSuspenseQuery<CompletePaymentData>({
+  return useSuspenseQuery<Reservation[], Error, CompletePaymentResponse>({
     queryKey: ["completedPayment"],
-    queryFn: async () =>
-      await getCompletedPayment({ cartIds, reservationName, headers }),
-    select: (rawData) => {
+    queryFn: async () => await getCompletedPayment({ count, headers }),
+    select: (reservations: Reservation[]): CompletePaymentResponse => {
       return {
-        reservationData: encodeReservationData(rawData),
-        rawData
+        reservationData: encodeReservationData(reservations),
+        totalPrice: encodeTotalPrice(reservations),
+        reservationName: encodeReservationName(reservations),
+        rawData: reservations
       };
     }
   });
-};
-
-const encodeReservationData = (data: any) => {
-  return [
-    { label: "숙소", value: data.data.product.hotel },
-    {
-      label: "객실",
-      value: data.data.product.room
-    }
-  ];
 };
