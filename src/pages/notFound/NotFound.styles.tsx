741db9300071e856886af9d36ab834be4ce2741e
JSH99 <tjgus9966@naver.com>
2023-11-29 18:52:28 +0900

    Feat: 404 페이지 추가

---
+++ src/pages/notFound/NotFound.styles.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 6rem;
+`;
+
+export const NotFoundTitle = styled.div`
+  text-align: center;
+  font-size: 1rem;
+  font-weight: 700;
+  color: #666666;
+  padding: 0.5rem;
+`;
+
+export const NotFoundMessage = styled.div`
+  text-align: center;
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 4.5rem;
+`;
