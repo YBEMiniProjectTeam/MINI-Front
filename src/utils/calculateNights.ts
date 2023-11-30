5f18e8f80a9d3559a1459d85068d65abdb378abe
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-12-01 04:49:02 +0900

    Fix: 숙박일 수 계산 함수 수정

--- src/utils/calculateNights.ts
+++ src/utils/calculateNights.ts
@@ -1,7 +1,28 @@
-export const calculateNights = (checkIn: string, checkOut: string) => {
-  const checkInDate = new Date(checkIn);
-  const checkOutDate = new Date(checkOut);
-  const difference = checkOutDate.getTime() - checkInDate.getTime();
-  const nights = difference / (1000 * 3600 * 24);
-  return nights;
+export const calculateNights = ({
+  checkInDate,
+  checkOutDate,
+  checkInTime,
+  checkOutTime
+}: {
+  checkInDate: string | undefined;
+  checkOutDate: string | undefined;
+  checkInTime: string | undefined;
+  checkOutTime: string | undefined;
+}) => {
+  const checkInDateTime = new Date(`${checkInDate} ${checkInTime}`).getTime();
+  const checkOutDateTime = new Date(
+    `${checkOutDate} ${checkOutTime}`
+  ).getTime();
+
+  const timeDiff = checkOutDateTime - checkInDateTime;
+
+  const hours = timeDiff / (1000 * 60 * 60);
+  const days = Math.floor(hours / 24);
+
+  if (days >= 1) {
+    return `${days}박`;
+  } else {
+    const remainingHours = Math.floor(hours);
+    return `${remainingHours}시간`;
+  }
 };
