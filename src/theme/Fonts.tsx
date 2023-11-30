2e41cd0ed7d2fdded14e26556f7372fb329f0fa2
lilviolie <lilviolie@gmail.com>
2023-11-30 17:18:17 +0900

    Design: 폰트 변경

--- src/theme/Fonts.tsx
+++ src/theme/Fonts.tsx
@@ -3,12 +3,9 @@ import { Global } from "@emotion/react";
 const Fonts = () => (
   <Global
     styles={`
-      /* latin */
-      @font-face {
-        font-family: 'Pretendard-Regular';
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-        font-weight: 400;
-        font-style: normal;
+    @font-face {
+      font-family: "Pretendard";
+      src: url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css");
     }
       `}
   />
