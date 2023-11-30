2244dac6d34a367b71047591f5a0aa1fe2f232ec
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-30 17:15:28 +0900

    Feat: 실제 API 엔드포인트 적용

--- src/api/getPaymentInfo.ts
+++ src/api/getPaymentInfo.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 import { API_BASE_URL } from "@api/config.ts";
 
 export const getPaymentInfo = async (
-  cartIds: number[],
+  cart_ids: number[],
   headers: { [key: string]: string }
 ) => {
-  const API_URL = `/api/carts/orders/reservations`;
-  const response = await axios.post(API_URL, { cartIds }, { headers });
+  const API_URL = `${API_BASE_URL}/carts/orders/reservations`;
+  const response = await axios.post(API_URL, { cart_ids }, { headers });
+  console.log(response);
   return response.data.data;
 };
