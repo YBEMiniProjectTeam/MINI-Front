81f96d816d034649ba1ac230ed09eb71d39b2a5f
seungsimdang <oak20005@naver.com>
2023-12-01 09:20:48 +0900

    HotFix: 타입 에러 해결

--- src/components/ChooseDateModal/ChooseDateModal.types.ts
+++ src/components/ChooseDateModal/ChooseDateModal.types.ts
@@ -1,7 +1,7 @@
 export interface DisclosureProps {
   isOpen: boolean;
   onClose: () => void;
-  setSelectedDate: React.Dispatch<React.SetStateAction<string[]>>;
+  setSelectedDate: React.Dispatch<React.SetStateAction<string[] | null>>;
   isFromSearchResult: boolean;
   personCount?: number;
   setPersonCount?: React.Dispatch<React.SetStateAction<number>>;
