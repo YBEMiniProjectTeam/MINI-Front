de7deccf4ac0d3d5b7c3385b3a9f3552c72fb6b4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-12-08 04:38:52 +0900

    Feat: AsyncWrapper 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import AsyncWrapper from "@components/AsyncWrapper";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Main } from "@pages/main/Main";
 import { Register } from "@pages/register/Register";
 import { Login } from "@pages/login/Login";
 import { SearchResult } from "@pages/searchResult/SearchResult";
 import { ProductDetail } from "@pages/productDetail/ProductDetail";
-import { Payment } from "@pages/payment/Payment";
-import CompletePayment from "@pages/completePayment/CompletePayment.tsx";
+import Payment from "@pages/payment/Payment";
+import CompletePayment from "@pages/completePayment/CompletePayment";
 import { ShoppingCart } from "@pages/shoppingCart/ShoppingCart";
 import { Reservations } from "@pages/reservations/Reservations";
 import { ReservationDetails } from "@pages/reservationDetails/ReservationDetails";
@@ -24,8 +25,22 @@ const router = createBrowserRouter([
       { path: "login", element: <Login /> },
       { path: "searchResult", element: <SearchResult /> },
       { path: "products", element: <ProductDetail /> },
-      { path: "orders", element: <Payment /> },
-      { path: "reservationComplete", element: <CompletePayment /> },
+      {
+        path: "orders",
+        element: (
+          <AsyncWrapper>
+            <Payment />
+          </AsyncWrapper>
+        )
+      },
+      {
+        path: "reservationComplete",
+        element: (
+          <AsyncWrapper>
+            <CompletePayment />
+          </AsyncWrapper>
+        )
+      },
       { path: "shoppingCart", element: <ShoppingCart /> },
       { path: "reservations", element: <Reservations /> },
       {
