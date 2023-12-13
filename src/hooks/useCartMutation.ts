b960240162e916fa432169288b8be18b4956967a
lilviolie <lilviolie@gmail.com>
2023-12-14 02:27:33 +0900

    Feat: 객실 장바구니 추가 시 toast 알림 추가

--- src/hooks/useCartMutation.ts
+++ src/hooks/useCartMutation.ts
@@ -4,13 +4,14 @@ import {
   ResponseType,
   PostCartProps
 } from "@components/ProductDetail/ChooseRoom/ChooseRoom.types";
+import { toast } from "react-hot-toast";
 
 export const usePostCart = () => {
   return useMutation<ResponseType, Error, PostCartProps>({
     mutationFn: ({ body, headers }: PostCartProps) =>
       postCart({ body, headers }),
-    onSuccess: (res) => {
-      console.log(res.statusCode, res.message);
+    onSuccess: () => {
+      toast.success("장바구니에 추가되었습니다.");
     },
     onError: (err) => {
       console.log(err);
