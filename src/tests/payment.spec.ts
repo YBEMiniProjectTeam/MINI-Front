1f3379892ff2d4a9b9bd3b730a1507d06f0eccc3
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-12-14 20:52:20 +0900

    Delete src/tests/payment.spec.ts

--- src/tests/payment.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("1. ", async ({ page }) => {
-  await page.goto("http://localhost:5173/shopping-cart");
-  
-  const cartItemText = await page.textContent(".roomListWrap h3");
-  expect(cartItemText).not.toBeNull();
-});
