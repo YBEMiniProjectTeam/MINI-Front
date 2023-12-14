85392cf35e77dd65b5b86f72a89029b57880d795
seungsimdang <oak20005@naver.com>
2023-12-14 22:30:42 +0900

    Feat: gpt api 연동

--- src/utils/isPrintable.tsx
+++ src/utils/isPrintable.tsx
@@ -1,11 +1,14 @@
 import { Box, Text, Divider } from "@chakra-ui/react";
+import { Nullable } from "@/types/nullable";
+import { Spinner } from "@chakra-ui/react";
+import * as styles from "@components/MyWishList/MyWishList.styles";
 
 const isPrintable = ({
   title,
   content
 }: {
   title: string;
-  content: string;
+  content: Nullable<string>;
 }) => {
   if (content !== ".") {
     return (
@@ -14,7 +17,19 @@ const isPrintable = ({
           {title}
         </Box>
         <Text fontSize="16px" marginBottom="3px" lineHeight={1.7}>
-          {content}
+          {content ? (
+            content
+          ) : (
+            <styles.SpinnerWrapper>
+              <Spinner
+                thickness="2px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="pink"
+                size="md"
+              />
+            </styles.SpinnerWrapper>
+          )}
         </Text>
         <Divider margin="40px 0" borderColor="#D9D9D9" />
       </>
