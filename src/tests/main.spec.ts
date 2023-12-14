7c19831026018bd15071a880c33d08683b24bde7
JSH99 <tjgus9966@naver.com>
2023-12-14 17:19:29 +0900

    Test: 숙소 그리드 뷰 테스트 코드 작성

--- src/tests/main.spec.ts
+++ src/tests/main.spec.ts
@@ -84,3 +84,39 @@ test("8. 숙소 캐러셀 상세 페이지로 이동", async ({ page }) => {
 
   expect(name).toContain("더메이호텔");
 }); 
+
+test("9. 숙소 그리드 뷰 호텔 탭으로 변경", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("tab").filter({ hasText: '호텔'}).click();
+
+  const nameElement = await page.locator("#grid-accommodation-name").first();
+  const name = await nameElement.textContent();
+
+  expect(name).toContain("메리어트관 제주신화월드 호텔 앤 리조트");
+});
+
+test("10. 숙소 그리드 뷰 검색 결과 페이지로 이동", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("button", { name: "모두 보기", exact: true }).nth(1).click();
+
+  // 카테고리
+  const categories = await page.getByRole("combobox");
+  await categories.selectOption({ index: 4 });
+  const selectedOption = await categories.inputValue();
+  
+  // 지역
+  const region = await page.locator(".css-1eziwv").first().textContent();
+
+  expect(selectedOption).toContain("펜션");
+  expect(region).toContain("서귀포시");
+});
+
+test("11. 숙소 그리드 뷰 상세 페이지로 이동", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.locator("#grid-accommodation-item").first().click();
+
+  const nameElement = page.locator(".css-1ebn8vi");
+  const name = await nameElement.textContent();
+
+  expect(name).toContain("제주캐슬펜션");
+});
