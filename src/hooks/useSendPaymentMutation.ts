3b3b6823fa41c1203c004c68ad5cef2eb939f765
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-12-15 08:36:33 +0900

    Feat: 결제 시 재고 없을 경우 에러 핸들링

--- src/hooks/useSendPaymentMutation.ts
+++ src/hooks/useSendPaymentMutation.ts
@@ -1,19 +1,39 @@
+import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 import type { ResponseType } from "@api/ApiResponse.types";
 import { postPayment, PaymentInfoProps } from "@api/postPayment";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 export const useSendPaymentMutation = () => {
   const navigate = useNavigate();
+
+  const handleUnexpectedStatusCode = (statusCode: number) => {
+    if (statusCode === 400) {
+      toast.error("재고가 부족합니다. 결제 요청이 실패했습니다.");
+    }
+    navigate(-1);
+  };
 
   return useMutation<ResponseType, Error, PaymentInfoProps>({
     mutationFn: ({ cart_ids, guest_name, guest_email }: PaymentInfoProps) =>
       postPayment({ cart_ids, guest_name, guest_email }),
 
     onSuccess: (res, variables) => {
-      console.log(res);
+      if (res.statusCode && res.statusCode !== 200) {
+        handleUnexpectedStatusCode(res.statusCode);
+        return;
+      }
       const orderId = `orderId=${variables.cart_ids.length}`;
       navigate(`/reservationComplete?${orderId}`);
+    },
+    onError: (err) => {
+      if (axios.isAxiosError(err)) {
+        const statusCode = err.response?.status;
+        handleUnexpectedStatusCode(statusCode!);
+      } else {
+        throw err;
+      }
     }
   });
 };
