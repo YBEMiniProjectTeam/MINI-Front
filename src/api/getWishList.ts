c859175abe29c1978d9348657b5cdc3364372204
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-12-15 22:41:48 +0900

    Fix: playwright setup 변경

--- src/api/getWishList.ts
+++ src/api/getWishList.ts
@@ -4,6 +4,6 @@ export const getWishList = async () => {
   const getWishListURL = `/wishes`;
 
   const response = await axiosInstance.get(getWishListURL);
-
+  console.log(axiosInstance);
   return response.data.data;
 };
--- src/tests/specs/authentication/login.spec.ts
+++ src/tests/specs/authentication/login.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+test.use({ storageState: { cookies: [], origins: [] } });
+
 test("로그인 성공", async ({ page }) => {
   await page.goto("http://localhost:5173/login");
 
